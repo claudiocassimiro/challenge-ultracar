3c6d5664dfc12cf29e568776e81a86ee38de1ffc
Claudio Cassimiro <days_surfing@live.com>
2023-04-17 21:44:36 -0300

    fix: hotfix

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -2,16 +2,16 @@ export const clients = [
   {
     id: "1",
     name: "Cláudio Cassimiro",
-    veiculo: "Jetta 2023",
+    vehicle: "Jetta 2023",
   },
   {
     id: "2",
     name: "Milena Mayara",
-    veiculo: "SW4 2023",
+    vehicle: "SW4 2023",
   },
   {
     id: "3",
     name: "Kiara Cassimiro",
-    veiculo: "BMW i3 2023",
+    vehicle: "BMW i3 2023",
   },
 ];
