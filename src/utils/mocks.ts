19c2262a99fabc6c12fec31499ffbfcdc63626e2
Claudio Cassimiro <days_surfing@live.com>
2023-04-17 21:03:47 -0300

    feat: implements mocks file

---
+++ src/utils/mocks.ts
@@ -0,0 +1,17 @@
+export const clients = [
+  {
+    id: "1",
+    name: "Cláudio Cassimiro",
+    veiculo: "Jetta 2023",
+  },
+  {
+    id: "2",
+    name: "Milena Mayara",
+    veiculo: "SW4 2023",
+  },
+  {
+    id: "3",
+    name: "Kiara Cassimiro",
+    veiculo: "BMW i3 2023",
+  },
+];
