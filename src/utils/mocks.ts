8b31d1b9bf93bb8d009e0dad503136bc2c5069b4
Claudio Cassimiro <days_surfing@live.com>
2023-04-17 22:41:11 -0300

    feat: add services and mechanics mock

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -15,3 +15,36 @@ export const clients = [
     vehicle: "BMW i3 2023",
   },
 ];
+
+export const services = [
+  {
+    id: "1",
+    service: "Troca de Oléo",
+    coast: 120,
+  },
+  {
+    id: "2",
+    service: "Troca de Pastilha de freio",
+    coast: 150,
+  },
+  {
+    id: "3",
+    service: "Troca de correia Dentada",
+    coast: 70,
+  },
+];
+
+export const mechanics = [
+  {
+    id: "1",
+    name: "Joab Silva",
+  },
+  {
+    id: "2",
+    name: "Marcos Costa",
+  },
+  {
+    id: "3",
+    name: "Mario Mariano",
+  },
+];
